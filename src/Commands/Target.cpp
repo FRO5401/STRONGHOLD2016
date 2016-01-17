4e8ab488d492c7c720a2dd4c89a79259c26081bd
kjm16216 <kmckay@bensalemsd.org>
2016-01-16 20:10:12 -0500

    Shooter file structure and pseudocode

---
+++ src/Commands/Target.cpp
@@ -0,0 +1,34 @@
+/*
+ * Catapult targeting for 2016 FRC Stronghold game
+ * Team 5401 Fightin' Robotic Owls
+ * FROGramming team
+ */
+
+#include "Target.h"
+#include "RobotMap.h"
+bool Lock;
+
+Target::Target()
+{
+  Requires(drivebase);
+}
+	void Target::Initialize() { };
+
+	void Target::Execute(){
+/*
+ * This will do image processing, to locate the target and properly move the robot to the launch site
+ * Then it will set a bool flag Lock to indicate target lock
+ * When finished, it will kick off the launch command
+ */
+	};
+
+	bool Target::IsFinished()
+{
+	return Lock;
+}
+
+	void Target::End(){
+		new Launch();
+	};
+
+	void Target::Interrupted(){};
---
+++ src/Subsystems/Shooter.cpp
@@ -0,0 +1,45 @@
+/*
+ * Catapult shooter for 2016 FRC Stronghold game
+ * Team 5401 Fightin' Robotic Owls
+ * FROGramming team
+ */
+
+#include "Shooter.h"
+#include "../RobotMap.h"
+
+Shooter::Shooter() :
+		Subsystem("Shooter")
+{
+	ShooterMotor	= new Victor(ShooterMotor_Channel);
+	ShooterEncoder	= new Encoder(Enc_Channel_A,Enc_Channel_B,true,Encoder::k1X);
+
+}
+
+void Shooter::InitDefaultCommand()
+{
+//	SetDefaultCommand(); //Not sure what/if the default command should be, but we may want a check/reset
+}
+
+void Shooter::Shoot() //Shoots the ball
+  {
+
+	/*
+ * Loop
+ * Check encoder is at its initial set point (cocked - constant)
+ * if not at cocked, reverse until at cocked
+ * else move to next set point (fired - constant), wait (wait time constant), and BREAK out of loop
+ * forward to cocked set point
+ * end
+ */
+
+  }
+
+  void Shooter::Override(double Input)
+  {
+
+/*
+ * Forward and reverse based on controller input
+ */
+
+  }
+
