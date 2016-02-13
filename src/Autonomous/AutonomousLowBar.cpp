620e5441d678888c12740a87977336fbdca7b843
jxfireruby <kmckay@HS-127-02.bensalemsd.org>
2016-02-13 10:01:57 -0500

    Added more parameters for the CommandGroup and organized where the autoMode selection should be

--- src/Autonomous/AutonomousLowBar.cpp
+++ src/Autonomous/AutonomousLowBar.cpp
@@ -7,6 +7,17 @@ AutonomousLowBar::AutonomousLowBar(int HighGoalPosition, int DefensePosition)
 	AddSequential(new LockTarget());//LockTarget needs it to be edited to find if it is within range
 
 	AddSequential(new Launch());
+
+/*	Pusedocode
+ * 	EncoderDrive() No parameter necessary. The placement will be the DistanceForEncoderDrive in DriveBase. COMMAND MADE
+ * 	AdjustAngle(0) Adjusts angle to 0 to make it run straight. COMMAND NOT CREATED YET
+ *	AutoTurn(45) Turns 45 degrees. VALUE MAY CHANGE. COMMAND NOT CREATED YET. May have parameters or might be like DistanceForEncoderDrive for EncoderDrive
+ *	AutoTarget() Finds target and locks on. Might be split into two command group. NEEDS EDITS
+ *	Launch() COMMAND MADE
+ *
+ */
+
+
 	// Add Commands here:
 	// e.g. AddSequential(new Command1());
 	//      AddSequential(new Command2());
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -93,6 +93,12 @@ private:
 	Command *autonomousCommand;
 	LiveWindow *lw;
 	SendableChooser *autoMode;
+	//Displays variables necessary for autonomous mode
+	//Positon for Goal. 1 for left. 2 for middle. 3 for right. Note that 2 will not be used for low goal because there is no middle low goal
+	//Defense positions refer to map of arena in game manual
+	int HighOrLowGoal		= 0;
+	int PositionForGoal 	= 0;
+	int PositionForDefense 	= 0;
 
 
 	void RobotInit()
@@ -100,14 +106,34 @@ private:
 		CommandBase::init();
 //		autonomousCommand = new ExampleCommand();
 		lw = LiveWindow::GetInstance();
-		int PositionForHighGoal;
-		int PositionForDefense;
-		//Displays variables necessary for autonomous mode
-		//Positon for High Goal. 1 for left. 2 for middle. 3 for right
-		SmartDashboard::PutNumber("Position of the High Goal", 	PositionForHighGoal);
+
+		SmartDashboard::PutNumber("Position of the Goal", 	PositionForGoal);
 		SmartDashboard::PutNumber("Position of the Defense", 	PositionForDefense);
-		//Takes in the variables necessary for autonomous mode
-		SmartDashboard::GetNumber("Position of the High Goal", 	PositionForHighGoal);
+/*			MOVED DOWN TO DISABLED PERIODIC()
+ *
+ *			autoMode = new SendableChooser();
+ *	//		Low Bar CommandGroup is constant because it'll always be in defense position 1 and go in left goal
+ *			autoMode->AddDefault("Default-Low Bar", new AutonomousLowBar(1,1));
+ *			autoMode->AddObject("Portcullis", new AutonomousPortcullis(PositionForHighGoal, PositionForDefense));
+ *			autoMode->AddObject("Cheval de Frise", new AutonomousChevalDeFrise(PositionForHighGoal, PositionForDefense));
+ *			autoMode->AddObject("Ramparts", new AutonomousRamparts(PositionForHighGoal, PositionForDefense));
+ *			autoMode->AddObject("Moat", new AutonomousMoat(PositionForHighGoal, PositionForDefense));
+ *	//		autoMode->AddObject("Drawbridge", new AutonomousDrawbridge(PositionForHighGoal, PositionForDefense));
+ *	//		autoMode->AddObject("Sally Port", new AutonomousSallyPort(PositionForHighGoal, PositionForDefense));
+ *			autoMode->AddObject("Rock Wall", new AutonomousRockWall(PositionForHighGoal, PositionForDefense));
+ *			autoMode->AddObject("Rough Terrain", new AutonomousRoughTerrain(PositionForHighGoal, PositionForDefense));
+ *	//		autoMode->AddObject("SpyBot", new AutonomousSpyBot()); //Restore when command is written, plus need one for each position
+ *			SmartDashboard::PutData("Autonomous Mode", autoMode);
+ */
+
+
+//		CameraServer::GetInstance() -> SetQuality(50);
+//		CameraServer::GetInstance() -> StartAutomaticCapture("cam0");
+	}
+	
+	void DisabledPeriodic()
+	{
+		SmartDashboard::GetNumber("Position of the High Goal", PositionForHighGoal);
 		SmartDashboard::GetNumber("Position of the Defense", 	PositionForDefense);
 		autoMode = new SendableChooser();
 		//Low Bar CommandGroup is constant because it'll always be in defense position 1 and go in left goal
@@ -122,15 +148,6 @@ private:
 		autoMode->AddObject("Rough Terrain", new AutonomousRoughTerrain(PositionForHighGoal, PositionForDefense));
 //		autoMode->AddObject("SpyBot", new AutonomousSpyBot()); //Restore when command is written, plus need one for each position
 		SmartDashboard::PutData("Autonomous Mode", autoMode);
-
-
-
-//		CameraServer::GetInstance() -> SetQuality(50);
-//		CameraServer::GetInstance() -> StartAutomaticCapture("cam0");
-	}
-	
-	void DisabledPeriodic()
-	{
 		Scheduler::GetInstance()->Run();
 	}
 
