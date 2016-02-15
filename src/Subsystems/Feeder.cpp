4b42b63052549249ab7473e75f42950060f8e43b
Kevin McKay <kmckay@bensalemsd.org>
2016-02-14 19:25:59 -0500

    Saturday debugging

--- src/Subsystems/Feeder.cpp
+++ src/Subsystems/Feeder.cpp
@@ -8,11 +8,15 @@
 #include "Feeder.h"
 #include "../RobotMap.h"
 
++const float InfeedGo	= 0.5; //Set max speed for infeed motors
++const float InfeedOut	= 0.9; //Set max speed for infeed motor kick
++const float DeliveryGo	= -0.5; //Set max speed for Delivery motors
+
 Feeder::Feeder() :
-		Subsystem("ExampleSubsystem")
+		Subsystem("Feeder")
 {
-	Victor *FeederOuterFasterMotor = new Victor(FeederOuterFasterMotor_Channel);
-	Victor *FeederInnerSlowerMotor = new Victor(FeederInnerSlower_Channel);
+	FeederOuterFasterMotor = new Victor(FeederOuterFasterMotor_Channel);
+	FeederInnerSlowerMotor = new Victor(FeederInnerSlower_Channel);
 }
 
 void Feeder::InitDefaultCommand()
@@ -25,21 +29,21 @@ void Feeder::InitDefaultCommand()
 // here. Call these from Commands.
 
 void Feeder::FeedInFromField(){
-	FeederOuterFasterMotor -> Set(1);
+	FeederOuterFasterMotor -> Set(InfeedGo);
 }
 
 void Feeder::FeedOutToField(){
-	FeederOuterFasterMotor -> Set(-1);
+	FeederOuterFasterMotor -> Set(InfeedOut);
 }
 
 void Feeder::FeedToShooter(){
-	FeederInnerSlowerMotor -> Set(0.5);
+	FeederInnerSlowerMotor -> Set(DeliveryGo);
 }
-
+/* KJM Not sure what this is.
 void Feeder::FeedOutFromShooter(){
 	FeederInnerSlowerMotor -> Set(-0.5);
 }
-
+*/
 void Feeder::StopFeed(){
 	FeederOuterFasterMotor -> Set(0);
 	FeederInnerSlowerMotor -> Set(0);
