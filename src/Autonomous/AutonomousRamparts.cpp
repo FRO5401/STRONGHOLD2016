eef280a2cc9ad8e339240ae6d585f1c2968ffb10
kjm16216 <kmckay@bensalemsd.org>
2016-02-20 11:26:45 -0500

    Finished PseudoCode 2.0 for Ramparts

--- src/Autonomous/AutonomousRamparts.cpp
+++ src/Autonomous/AutonomousRamparts.cpp
@@ -1,8 +1,8 @@
 #include "Autonomous/AutonomousRamparts.h"
-
+//Include the Subsystems required
 AutonomousRamparts::AutonomousRamparts(int DefensePosition, int GoalPosition, int LowOrHigh)
 {
-	/* Pseudocode
+	/* Pseudocode Don't Use
 	 * EncoderDrive() Needs a value for DistanceForEncoderDrive
 	 * AdjustAngle to 0.
 	 * if(GoalPosition == 1)
@@ -21,10 +21,10 @@ AutonomousRamparts::AutonomousRamparts(int DefensePosition, int GoalPosition, in
 	 *						//Adjust angle
 	 *						//Turn 45 Right
 	 *						//LockTarget
-	 *						//Launch
+	 *						//OutFeed
 	 *						break;
 	 *					case 2:
-	 *						//Turn right 90 degrees
+	 *						//Turn Left 90 degrees
 	 *						//EncoderDrive(some inches)
 	 *						//Adjust angle
 	 *						//Turn Left 90 degrees
@@ -67,7 +67,249 @@ AutonomousRamparts::AutonomousRamparts(int DefensePosition, int GoalPosition, in
 	 * }
 	 *
 	 */
-
+	/*	PsuedoCode V. 2.0
+	 * 	AdjustAngle() so it's straight
+	 * 	AutoDrive()
+	 * 	AdjustAngle() so it's straight
+	 * 	switch(DefensePosition)
+	 * 	{
+	 * 		case 2:
+	 * 			switch(GoalPosition)
+	 * 			{
+	 * 				case 1:
+	 * 					AutoTurn() left 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() right 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AdjustAngle straight
+	 * 					AutoTutn() turns directly towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To front of low goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 				case 2:
+	 * 					AutoTurn() Right 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					//Maybe loop between AutoDrive and AutoTarget
+	 * 					AutoTarget();
+	 * 					Shoot();
+	 * 					break;
+	 * 					}
+	 * 					break;
+	 * 				case 3:
+	 * 					AutoTurn() Right 90
+	 * 					AutoDrive() until it goes over the secret passage boundary
+	 * 					AdjustAngle straight for after going over boundary
+	 * 					AutoDrive() for the rest of the distance after boundary
+	 * 					AdjustAngle() Make sure it's straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AutoTurn() turns towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To In front of goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 			}
+	 * 			break;
+	 * 		case 3:
+	 * 			switch(GoalPosition)
+	 * 			{
+	 * 				case 1:
+	 * 					AutoTurn() left 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() right 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AdjustAngle straight
+	 * 					AutoTutn() turns directly towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To front of low goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 				case 2:
+	 * 					AutoTurn() Right 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					//Maybe loop between AutoDrive and AutoTarget
+	 * 					AutoTarget();
+	 * 					Shoot();
+	 * 					break;
+	 * 				case 3:
+	 * 					AutoTurn() Right 90
+	 * 					AutoDrive() until it goes over the secret passage boundary
+	 * 					AdjustAngle straight for after going over boundary
+	 * 					AutoDrive() for the rest of the distance after boundary
+	 * 					AdjustAngle() Make sure it's straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AutoTurn() turns towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To In front of goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 			}
+	 * 			break;
+	 * 		case 4:
+	 * 			switch(GoalPosition)
+	 * 			{
+	 * 				case 1:
+	 * 					AutoTurn() left 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() right 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AdjustAngle straight
+	 * 					AutoTutn() turns directly towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To front of low goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 				case 2:
+	 * 					AutoTurn() left 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					//Maybe loop between AutoDrive and AutoTarget
+	 * 					AutoTarget();
+	 * 					Shoot();
+	 * 					break;
+	 * 				case 3:
+	 * 					AutoTurn() Right 90
+	 * 					AutoDrive() until it goes over the secret passage boundary
+	 * 					AdjustAngle straight for after going over boundary
+	 * 					AutoDrive() for the rest of the distance after boundary
+	 * 					AdjustAngle() Make sure it's straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AutoTurn() turns towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To In front of goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 			}
+	 * 			break;
+	 * 		case 5:
+	 * 			switch(GoalPosition)
+	 * 			{
+	 * 				case 1:
+	 * 					AutoTurn() left 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() right 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AdjustAngle straight
+	 * 					AutoTutn() turns directly towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To front of low goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 				case 2:
+	 * 					AutoTurn() left 90
+	 * 					AutoDrive()
+	 * 					AdjustAngle straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					//Maybe loop between AutoDrive and AutoTarget
+	 * 					AutoTarget();
+	 * 					Shoot();
+	 * 					break;
+	 * 				case 3:
+	 * 					AutoTurn() Right 90
+	 * 					AutoDrive() until it goes over the secret passage boundary
+	 * 					AdjustAngle straight for after going over boundary
+	 * 					AutoDrive() for the rest of the distance after boundary
+	 * 					AdjustAngle() Make sure it's straight
+	 * 					AutoTurn() Left 90
+	 * 					AutoDrive() up to appropriate distance from goal
+	 * 					AutoTurn() turns towards goal
+	 * 					switch(LowOrHigh)
+	 * 					{
+	 * 						case 1:
+	 * 							AutoDrive(); To In front of goal
+	 * 							OutFeed(); AKA low kick
+	 * 							break;
+	 * 						case 2:
+	 * 						//Maybe loop between AutoDrive and AutoTarget
+	 * 							AutoTarget();
+	 * 							Shoot();
+	 * 							break;
+	 * 					}
+	 * 					break;
+	 * 			}
+	 * 			break;
+	 * 	}
+	 *
+	 */
 
 	// Add Commands here:
 	// e.g. AddSequential(new Command1());
