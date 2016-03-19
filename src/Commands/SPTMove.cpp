85db421ee304369ebdbad788f169926eb8fe13d9
kjm16216 <kmckay@bensalemsd.org>
2016-03-18 20:03:07 -0400

    Tried Fixing the Inactive UpAndDownSPT
    Tempered with SPTMove.cpp and OI.cpp

--- src/Commands/SPTMove.cpp
+++ src/Commands/SPTMove.cpp
@@ -15,7 +15,7 @@ void SPTMove::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void SPTMove::Execute()
 {
-	bool SPT_Override = (oi	->	GetMOHButtonL3());
+	bool SPT_Override = (oi	->	GetMOHRightStickButton());
 	double UpOrDownValue = oi -> GetUpOrDownValueInfeeder();
 
 	//Tells the Infeeder to go up or down
